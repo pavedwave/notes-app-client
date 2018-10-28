1dba44f95b4818c5f872754f208b3c2d8706a226
James Peters <pavedwave@gmail.com>
2018-10-28 08:16:44 -0700

    Create_Login_Page

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-mybucket2"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "n907z4hiwj"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_GLTwaT4Qi",
+      APP_CLIENT_ID: "7eiah03hn52bt317slqrebleo1",
+      IDENTITY_POOL_ID: "us-east-2:8dd0e948-42b8-491b-9c2a-a077aec02c6c"
+    }
+  };
